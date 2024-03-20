2cdf22dcf02b0bc718cfc24b10a8025ae6373aeb
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-03-19 22:09:15 -0300

    novas aulas adicionadas

---
+++ Declarações/js/script.js
@@ -0,0 +1,28 @@
+let a,b,c;
+
+a = 5;
+b = 6;
+c = 7;
+
+//não é obrigado colocar ponto e virgulas, mas e sempre bom colocar  , as declarações são separadas por pontos e virgulas (;)
+
+
+
+
+
+
+let pessoa1 = "nome";
+let pessoa2="nome";    //nao tem problema colocar ou não espaço em branco
+
+
+
+
+
+
+
+window.document.getElementById('texto').innerHTML=
+"Meu primeiro texto em <strong>JS!</strong>";  //nao tem problema quebrar as linhas no javascript (mas para isso devemos sempre colocar ponto e virgula no final(;))
+
+
+
+
---
+++ aula01 tipos de saidas/js/script.js
@@ -0,0 +1,4 @@
+window.document.getElementById('texto').innerHTML="Meu primeiro texto em <strong>JS!</strong>";
+
+window.alert('isso é um alerta');
+console.log('oi isso e um console.log');
