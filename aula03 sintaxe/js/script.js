00e1b5aa4cc70d16c4200208127a93557590076e
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-03-19 22:24:48 -0300

    nova aula adicionada

---
+++ aula03 sintaxe/js/script.js
@@ -0,0 +1,21 @@
+console.log('5 + 5'); //string
+console.log(5 + 5); //Number
+
+
+
+var a = 1;
+var b = 5;
+var c = a * b;
+console.log(c);
+
+
+
+var pessoa = "nome minusculo";
+var PESSOA = "nome maiusculo";  //javascript é case sensitive (sensivel a maiusculo e minusculo)
+
+
+
+
+var primeiro_nome;
+var primeiroNome;  //podemos usar underline ou letras maiusculas para simular um espaço nas variaveis
+
